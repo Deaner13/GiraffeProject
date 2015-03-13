8f603bf1d398558ce59097734578e1eb4c1ba580
Deaner13 <DeaneCarlo@gmail.com>
2015-03-13 14:02:58 -0600

    Making small progress

--- src/giraffe/Giraffe2.java
+++ src/giraffe/Giraffe2.java
@@ -1,10 +1,9 @@
 package giraffe;
 
-public class Giraffe2 {
+public class Giraffe2 extends Object {
 	private String birthLocation, sire, dam, subSpecies, zoo, city, state, event, name,
-	localId, birthDate, sex, giraffeId;
-	//protected int giraffeId;
-	
+	localId, birthDate, sex, giraffeId, eachGiraffe;
+	private int atCMZ, howManyGiraffes;
 	
 	public Giraffe2(String array[]){
 		this.giraffeId = array[0];
@@ -21,21 +20,69 @@ public class Giraffe2 {
 		this.state = array[11];
 		this.event = array[12];
 		
+		this.giraffeId = String.format("%-4s",this.giraffeId);
+		this.sex = String.format("%-1s",this.sex);
+		this.birthDate = String.format("%-10s",this.birthDate);
+		this.sire = String.format("%-4s",this.sire);
+		this.dam = String.format("%-4s",this.dam);
+		this.birthLocation = String.format("%-12s",this.birthLocation);
+		this.localId = String.format("%-7s",this.localId);
+		this.name = String.format("%-20s",this.name);
+		this.subSpecies = String.format("%-14s",this.subSpecies);
+		this.zoo = String.format("%-35s",this.zoo);
+		this.city = String.format("%-17s",this.city);
+		this.state = String.format("%-13s",this.state);
+		this.event = String.format("%-7s",this.event);
+		
+		for(int i = 0; i < array.length; i++){
+			this.howManyGiraffes++;
+		}
+		
+		if(array[9] == "Cheyenne Mtn Zoological Park"){
+			this.atCMZ++; 
+			System.out.println("This:" + this.atCMZ);
+		}
 	
 	}
 	
-	public void printData(){
-		System.out.println(this.giraffeId + " " + this.sex + " " + this.birthDate + " " + this.sire +
-				" " + this.dam + " " + this.birthLocation + " " + this.localId + " " + this.name + 
-				" " + this.subSpecies + " " + this.zoo + " " + this.city + " " + this.state + " " + 
+	public void format(Giraffe giraffe){
+		
+		this.giraffeId = String.format("%-4s",this.giraffeId);
+		this.sex = String.format("%-1s",this.sex);
+		this.birthDate = String.format("%-10s",this.birthDate);
+		this.sire = String.format("%-4s",this.sire);
+		this.dam = String.format("%-4s",this.dam);
+		this.birthLocation = String.format("%-12s",this.birthLocation);
+		this.localId = String.format("%-7s",this.localId);
+		this.name = String.format("%-20s",this.name);
+		this.subSpecies = String.format("%-14s",this.subSpecies);
+		this.zoo = String.format("%-35s",this.zoo);
+		this.city = String.format("%-17s",this.city);
+		this.state = String.format("%-13s",this.state);
+		this.event = String.format("%-7s",this.event);
+		
+	}
+	
+	public String toString(){
+		eachGiraffe = (this.giraffeId + " " +
+				this.sex + " " +
+				this.birthDate + " " +
+				this.sire +
+				" " +
+				this.dam + " " +
+				this.birthLocation + " " +
+				this.localId + " " +
+				this.name + 
+				" " + 
+				this.subSpecies + " " +
+				this.zoo + " " +
+				this.city + " " +
+				this.state + " " + 
 				this.event);
+		return eachGiraffe;
+		
 	}
-			//public Giraffe2(String giraffeId, String sex, String birthDate, String sire, String dam,
-			//	String birthLocation, String localId, String name, String subSpecies, String zoo,
-			//String city, String state, String event){
 			
-
-	
 }
 /*
 public void Giraffe2(int giraffeId, String sex, String birthDate, String sire, String dam,
--- src/giraffe/HW3_Carlo.java
+++ src/giraffe/HW3_Carlo.java
@@ -1,45 +1,52 @@
 package giraffe;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//String firstLine = in.nextLine();
-//System.out.println(firstLine);
-//System.out.println(" ");
-
 public class HW3_Carlo {
 
 	public static void main(String[] args) throws IOException {
+		//Create an array structures that will hold the Giraffes
 		ArrayList<Giraffe2> giraffes = new ArrayList<>();
-		String line;
+		Object[] giraffes2;
+		//Giraffe2[] giraffes3;
+		
+		//Create an array that will hold tab-del values
 		String temp[] = new String[13];
-		int i = 0;
-
+		String header = "";
+		//Define the file that will be used for creating objects
 		String fileLocation = "theHerd.txt";
 		File textFile = new File(fileLocation);
 		
+		//Populate the giraffes ArrayList with items from the text file
+		int i = 0;
 		while(i < 90) {
 		if (textFile.canRead()) {
 			Scanner in = new Scanner(textFile);
 			in = new Scanner(textFile);
-			while (in.hasNextLine()) {
-				temp = in.nextLine().split("\\t", 13);
-				giraffes.add(new Giraffe2(temp));
-				
-				//for (int i = 0; i < temp.length; i++) {
-					//giraffes.add(new Giraffe2(temp));
-					//i++;
-				//}
-				i++;
-				in.close();
-			}
+		//Take the first line of the file and make it a header
+			header = in.nextLine();
 			
-			in.close();
+			while (in.hasNextLine()) {
+		//Split each line at every tab (13 times), store in array temp
+				temp = in.nextLine().split("\\t", 13);
+		//Create a new giraffe from the array of strings in temp
+				giraffes.add(new Giraffe2(temp));
+				i++;
+				}
+			
+			}
 		}
-		System.out.println();
+		
+		//Convert ArrayList to array
+		giraffes2 =  giraffes.toArray();
+		System.out.println(header);
+		System.out.println(" ");
+		//Print out t
+		for(i = 0; i < giraffes2.length; i++){
+			System.out.println(giraffes2[i]);
 		}
 	}
 }
